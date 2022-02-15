25cbdcc886da197473a008429bad05f55b3bc670
stephen talley <stvlley.amzn@gmail.com>
2022-02-15 09:36:34 -0600

    CLEARED HTML AND CSS

--- script.js
+++ script.js
@@ -5,9 +5,5 @@ fetch("https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_mi
 		"x-rapidapi-key": "c6b32db4b5msha769fbc18ffde82p16fbe4jsn6b6843eb976c"
 	}
 })
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+.then((resp) => resp.json())
+.then((data) => {console.log(data)})
