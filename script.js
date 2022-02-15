40ab87314b083b7811debe961469c78b1dfd5392
stephen talley <stvlley.amzn@gmail.com>
2022-02-15 14:30:30 -0600

    addded names to img tags and changes background in css

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
     fetch("https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "tasty.p.rapidapi.com",
-		"x-rapidapi-key": "c6b32db4b5msha769fbc18ffde82p16fbe4jsn6b6843eb976c"
-	}
-    
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "tasty.p.rapidapi.com",
+        "x-rapidapi-key": "c6b32db4b5msha769fbc18ffde82p16fbe4jsn6b6843eb976c"
+    }
     })
     .then((resp) => resp.json())
     .then((recipes) => getNameAndImage(recipes))
-    
 })
-
 function getNameAndImage(recipes){
-    let recipeResult = recipes.result
-    console.log(recipeResult)
+    const recipeResult = recipes.results
+    recipeResult.forEach( plate => {
+        //make a p and assign it with varible plate's name
+        const img = document.createElement("img")
+        img.setAttribute("src", plate.thumbnail_url )
+        
+        const container = document.querySelector("#recipe-container")
+        
+        container.append(img)
+        img.innerHTML = plate.name
+        container.append(plate.name)
+        const instructions = plate.instructions
+        
+    });
+
 }
